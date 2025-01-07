9d25d7ec881a420ab8cc1b3a0b0b2cf52ed81a4e
cvfisher <cvfisher.93@gmail.com>
2025-01-07 20:25:50 +0000

    add form, validations, modal loading animations and text

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const modal = document.getElementById("modal");
+const modalCloseBtn = document.getElementById("modal-close-btn");
+const consentForm = document.getElementById("consent-form");
+const modalText = document.getElementById("modal-text");
 
 setTimeout(function () {
-	modal.classList.remove("hidden");
+	modal.style.display = "inline";
 }, 1500);
+
+modalCloseBtn.addEventListener("click", () => {
+	modal.style.display = "none";
+});
+
+consentForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	modalText.innerHTML = `<div class="modal-inner-loading">
+    <img src="images/loading.svg" class="loading">
+    <p id="uploadText">
+        Uploading your data to the dark web...
+    </p>
+</div>`;
+	setTimeout(() => {
+		document.getElementById("uploadText").innerText = `Making the sale...`;
+	}, 1500);
+	console.log("form submitted");
+});
